f28a8764c2424e836cc0cc1f56de62d6ed321b89
Andres Parra <byandrev@gmail.com>
2025-11-29 14:28:57 -0500

    refactor(domain): update UserUseCase to integrate encrypt password

--- src/main/java/com/pragma/powerup/domain/usecase/UserUseCase.java
+++ src/main/java/com/pragma/powerup/domain/usecase/UserUseCase.java
@@ -2,20 +2,22 @@ package com.pragma.powerup.domain.usecase;
 
 import com.pragma.powerup.domain.api.IUserServicePort;
 import com.pragma.powerup.domain.model.UserModel;
+import com.pragma.powerup.domain.spi.IPasswordEncoderPort;
 import com.pragma.powerup.domain.spi.IUserPersistencePort;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 public class UserUseCase implements IUserServicePort {
 
     private final IUserPersistencePort userPersistencePort;
-
-    public UserUseCase(IUserPersistencePort userPersistencePort) {
-        this.userPersistencePort = userPersistencePort;
-    }
+    private final IPasswordEncoderPort passwordEncoderPort;
 
     @Override
     public void saveUser(UserModel user) {
+        String hashedPassword = passwordEncoderPort.encode(user.getClave());
+        user.setClave(hashedPassword);
         userPersistencePort.saveUser(user);
     }
 
