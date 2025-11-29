8d6ed55d097e0edee9afc22bf9b269030d6d6fbf
Andres Parra <byandrev@gmail.com>
2025-11-29 13:18:30 -0500

    feat(infrastructure): add JPA entity, repository and persistence adapter for user

---
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/UserJpaAdapter.java
@@ -0,0 +1,46 @@
+package com.pragma.powerup.infrastructure.out.jpa.adapter;
+
+import com.pragma.powerup.domain.model.UserModel;
+import com.pragma.powerup.domain.spi.IUserPersistencePort;
+import com.pragma.powerup.infrastructure.exception.NoDataFoundException;
+import com.pragma.powerup.infrastructure.out.jpa.entity.UserEntity;
+import com.pragma.powerup.infrastructure.out.jpa.mapper.IUserEntityMapper;
+import com.pragma.powerup.infrastructure.out.jpa.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class UserJpaAdapter implements IUserPersistencePort {
+
+    private final IUserRepository userRepository;
+    private final IUserEntityMapper userEntityMapper;
+
+
+    @Override
+    public UserModel saveUser(UserModel user) {
+        UserEntity userEntity = userRepository.save(userEntityMapper.toEntity(user));
+        return userEntityMapper.toUserModel(userEntity);
+    }
+
+    @Override
+    public List<UserModel> getAllUsers() {
+        List<UserEntity> usersEntityList = userRepository.findAll();
+        
+        if (usersEntityList.isEmpty()) {
+            throw new NoDataFoundException();
+        }
+
+        return userEntityMapper.toUserModelList(usersEntityList);
+    }
+
+    @Override
+    public UserModel getUserById(Long id) {
+        Optional<UserEntity> userEntity = userRepository.findById(id);
+        
+        if (userEntity.isEmpty()) throw new NoDataFoundException();
+        
+        return userEntityMapper.toUserModel(userEntity.get());
+    }
+}
---
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/UserEntity.java
@@ -0,0 +1,46 @@
+package com.pragma.powerup.infrastructure.out.jpa.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "usuarios")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String nombre;
+
+    @Column(length = 50, nullable = false)
+    private String apellido;
+
+    @Column(length = 50, name = "numero_documento", nullable = false)
+    private String numeroDocumento;
+
+    @Column(length = 13, nullable = false)
+    private String celular;
+
+    @Column(name = "fecha_nacimiento", nullable = false)
+    private LocalDate fechaNacimiento;
+
+    @Column(length = 255, nullable = false)
+    private String correo;
+
+    @Column(nullable = false)
+    private String clave;
+
+
+}
