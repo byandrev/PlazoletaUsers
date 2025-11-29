17af8b600333ac13427ed37a9eda3a69d487a7a4
Andres Parra <byandrev@gmail.com>
2025-11-29 16:23:04 -0500

    docs(api): add Swagger documentation for user REST endpoints

--- src/main/java/com/pragma/powerup/infrastructure/input/rest/UserRestController.java
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/UserRestController.java
@@ -3,6 +3,12 @@ package com.pragma.powerup.infrastructure.input.rest;
 import com.pragma.powerup.application.dto.request.UserRequestDto;
 import com.pragma.powerup.application.dto.response.UserResponseDto;
 import com.pragma.powerup.application.handler.IUserHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +24,24 @@ public class UserRestController {
 
     private final IUserHandler userHandler;
 
+    @Operation(summary = "Add a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created", content = @Content),
+            @ApiResponse(responseCode = "409", description = "User already exists", content = @Content)
+    })
     @PostMapping("/")
     public ResponseEntity<Void> saveUser(@Valid @RequestBody UserRequestDto userRequestDto) {
         userHandler.saveUser(userRequestDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All users returned",
+                    content = @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = UserResponseDto.class)))),
+            @ApiResponse(responseCode = "404", description = "No data found", content = @Content)
+    })
     @GetMapping("/")
     public ResponseEntity<List<UserResponseDto>> getAll() {
         return ResponseEntity.ok(userHandler.getAllUsers());
