0ba5132a5167c4e4877875740a3346599691b08c
Andres Parra <byandrev@gmail.com>
2025-11-29 13:18:40 -0500

    feat(infrastructure): add REST controller for user endpoints

---
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/UserRestController.java
@@ -0,0 +1,31 @@
+package com.pragma.powerup.infrastructure.input.rest;
+
+import com.pragma.powerup.application.dto.request.UserRequestDto;
+import com.pragma.powerup.application.dto.response.UserResponseDto;
+import com.pragma.powerup.application.handler.IUserHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserRestController {
+
+    private final IUserHandler userHandler;
+
+    @PostMapping("/")
+    public ResponseEntity<Void> saveUser(@RequestBody UserRequestDto userRequestDto) {
+        userHandler.saveUser(userRequestDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserResponseDto>> getAll() {
+        return ResponseEntity.ok(userHandler.getAllUsers());
+    }
+
+}
