ce0c85028bbef5cd4d06f70ace6a28fc0ea66c5c
Andres Parra <byandrev@gmail.com>
2025-11-29 20:08:39 -0500

    chore: add insert to create test client

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,5 @@ INSERT INTO roles (nombre, descripcion) VALUES ('ADMINISTRADOR', 'Admin') ON CON
 INSERT INTO roles (nombre, descripcion) VALUES ('PROPIETARIO', 'Propietario de un establecimiento') ON CONFLICT (nombre) DO NOTHING;
 INSERT INTO roles (nombre, descripcion) VALUES ('EMPLEADO', 'Empleado de un establecimiento') ON CONFLICT (nombre) DO NOTHING;
 INSERT INTO roles (nombre, descripcion) VALUES ('CLIENTE', 'Cliente de la plazoleta') ON CONFLICT (nombre) DO NOTHING;
+
+INSERT INTO usuarios (nombre, apellido, numero_documento, celular, fecha_nacimiento, correo, clave, id_rol) VALUES ('Cliente Test', 'Test', '123456789', '+57123456789', '2000-5-19', 'test@gmail.com', '$2a$10$IsNGpLkS0LnFaP.TMMBMuu/ssdfnBGA8UcXmQa4CA1BNNyRDyjNbe', 4) ON CONFLICT (correo) DO NOTHING;
