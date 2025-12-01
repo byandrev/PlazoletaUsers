f4384d5b8c4663a1f88d86b5c616b830720fbe71
Andres Parra <byandrev@gmail.com>
2025-12-01 16:34:59 -0500

    chore: update data.sql for create admin user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO roles (nombre, descripcion) VALUES ('PROPIETARIO', 'Propietario de u
 INSERT INTO roles (nombre, descripcion) VALUES ('EMPLEADO', 'Empleado de un establecimiento') ON CONFLICT (nombre) DO NOTHING;
 INSERT INTO roles (nombre, descripcion) VALUES ('CLIENTE', 'Cliente de la plazoleta') ON CONFLICT (nombre) DO NOTHING;
 
-INSERT INTO usuarios (nombre, apellido, numero_documento, celular, fecha_nacimiento, correo, clave, id_rol) VALUES ('Cliente Test', 'Test', '123456789', '+57123456789', '2000-5-19', 'test@gmail.com', '$2a$10$IsNGpLkS0LnFaP.TMMBMuu/ssdfnBGA8UcXmQa4CA1BNNyRDyjNbe', 4) ON CONFLICT (correo) DO NOTHING;
+INSERT INTO usuarios (nombre, apellido, numero_documento, celular, fecha_nacimiento, correo, clave, id_rol) VALUES ('Admin', 'Admin', '12345', '+5712345', '2000-5-19', 'admin@gmail.com', '$2a$10$IsNGpLkS0LnFaP.TMMBMuu/ssdfnBGA8UcXmQa4CA1BNNyRDyjNbe', 1) ON CONFLICT (correo) DO NOTHING;
