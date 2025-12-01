18500e3b37605f9ef9aaa6fe264fd95cca0190eb
Andres Parra <byandrev@gmail.com>
2025-12-01 16:34:01 -0500

    feat(infrastructure): add JwtUtils for token generation and validation

---
+++ src/main/java/com/pragma/powerup/infrastructure/security/jwt/JwtUtils.java
@@ -0,0 +1,43 @@
+package com.pragma.powerup.infrastructure.security.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(String userName) {
+        return JWT.create()
+                .withSubject(userName)
+                .withIssuer("plazoleta")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(expiration)))
+                .sign(Algorithm.HMAC256(secret));
+    }
+
+    public Boolean verifyToken(String token) {
+        try {
+            JWT.require(Algorithm.HMAC256(secret)).build().verify(token);
+            return true;
+        } catch(JWTVerificationException exception) {
+            return false;
+        }
+    }
+
+    public String getUsername(String token) {
+        return JWT.require(Algorithm.HMAC256(secret)).build().verify(token).getSubject();
+    }
+
+}
