911f0c8e21d369036df03c6634c20dbfd7a9b3c4
Andres Parra <byandrev@gmail.com>
2025-12-01 16:34:14 -0500

    feat(application): add UserLoginDto and UserLoggedDto

---
+++ src/main/java/com/pragma/powerup/application/dto/request/UserLoginDto.java
@@ -0,0 +1,18 @@
+package com.pragma.powerup.application.dto.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class UserLoginDto {
+
+    @NotBlank(message = "El correo no puede estar vacio")
+    @Email(message = "Correo inválido")
+    private String correo;
+
+    @NotBlank(message = "La clave no puede estar vacia")
+    private String clave;
+
+}
