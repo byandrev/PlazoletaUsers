62b1360bb61230b0c6d702118c7167c77244a205
Andres Parra <byandrev@gmail.com>
2025-12-01 16:34:35 -0500

    feat(infra): add CustomUserDetailsService for JWT authentication

---
+++ src/main/java/com/pragma/powerup/infrastructure/security/authentication/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.pragma.powerup.infrastructure.security.authentication;
+
+import com.pragma.powerup.domain.model.UserModel;
+import com.pragma.powerup.domain.spi.IUserPersistencePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final IUserPersistencePort userPersistencePort;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel userModel = userPersistencePort.getByCorreo(username);
+
+        return User.builder()
+                .username(userModel.getCorreo())
+                .password(userModel.getClave())
+                .roles(String.valueOf(userModel.getRol().getNombre()))
+                .build();
+    }
+
+}
