9e28f2061c28717e62398596aec861054bd0c529
Andres Parra <byandrev@gmail.com>
2025-12-02 18:57:15 -0500

    refactor(infra): simplify BeanConfiguration by enabling auto bean detection

--- src/main/java/com/pragma/powerup/infrastructure/configuration/BeanConfiguration.java
+++ src/main/java/com/pragma/powerup/infrastructure/configuration/BeanConfiguration.java
@@ -1,49 +1,10 @@
 package com.pragma.powerup.infrastructure.configuration;
 
-import com.pragma.powerup.domain.api.IUserServicePort;
-import com.pragma.powerup.domain.spi.IPasswordEncoderPort;
-import com.pragma.powerup.domain.spi.IRolPersistencePort;
-import com.pragma.powerup.domain.spi.IUserPersistencePort;
-import com.pragma.powerup.domain.usecase.UserUseCase;
-import com.pragma.powerup.infrastructure.out.jpa.adapter.RolJpaAdapter;
-import com.pragma.powerup.infrastructure.out.jpa.adapter.UserJpaAdapter;
-import com.pragma.powerup.infrastructure.out.jpa.mapper.IRolEntityMapper;
-import com.pragma.powerup.infrastructure.out.jpa.mapper.IUserEntityMapper;
-import com.pragma.powerup.infrastructure.out.jpa.repository.IRolRepository;
-import com.pragma.powerup.infrastructure.out.jpa.repository.IUserRepository;
-import com.pragma.powerup.infrastructure.security.PasswordEncoderAdapter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class BeanConfiguration {
-
-    private final IUserRepository  userRepository;
-    private final IUserEntityMapper userEntityMapper;
-
-    private final IRolRepository  rolRepository;
-    private final IRolEntityMapper rolEntityMapper;
-
-    @Bean
-    public IPasswordEncoderPort bCryptPasswordEncoder() {
-        return new PasswordEncoderAdapter();
-    }
-
-    @Bean
-    public IUserPersistencePort userPersistencePort() {
-        return new UserJpaAdapter(userRepository, userEntityMapper);
-    }
-
-    @Bean
-    public IRolPersistencePort  rolPersistencePort() {
-        return new RolJpaAdapter(rolRepository, rolEntityMapper);
-    }
-
-    @Bean
-    public IUserServicePort userServicePort() {
-        return new UserUseCase(userPersistencePort(), rolPersistencePort(), bCryptPasswordEncoder());
-    }
 
 }
