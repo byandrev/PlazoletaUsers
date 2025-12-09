2afb14d9135e129e5719965d73ba2178e46178af
Andres Parra <byandrev@gmail.com>
2025-12-08 21:57:40 -0500

    refactor(dto): add Lombok builder pattern to UserRequestDto and UserResponseDto

--- src/main/java/com/pragma/powerup/application/dto/response/UserResponseDto.java
+++ src/main/java/com/pragma/powerup/application/dto/response/UserResponseDto.java
@@ -1,6 +1,7 @@
 package com.pragma.powerup.application.dto.response;
 
 import com.pragma.powerup.domain.model.RolModel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@Builder
 public class UserResponseDto {
 
     private Long id;
